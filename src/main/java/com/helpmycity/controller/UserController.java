c166deebff05d7e5cb3dca588bdca005e9451bcf
hechmi <elhechmi.essalah@digitus.tech>
2018-06-08 22:59:03 +0100

    update user

--- src/main/java/com/helpmycity/controller/UserController.java
+++ src/main/java/com/helpmycity/controller/UserController.java
@@ -2,11 +2,15 @@ package com.helpmycity.controller;
 
 import com.helpmycity.exception.ResourceNotFoundException;
 import com.helpmycity.model.Reclamation;
+import com.helpmycity.model.Role;
+import com.helpmycity.model.RoleName;
 import com.helpmycity.model.User;
+import com.helpmycity.payload.ApiResponse;
 import com.helpmycity.payload.UserIdentityAvailability;
 import com.helpmycity.payload.UserProfile;
 import com.helpmycity.payload.UserSummary;
 import com.helpmycity.repository.ReclamationRepository;
+import com.helpmycity.repository.RoleRepository;
 import com.helpmycity.repository.UserRepository;
 import com.helpmycity.security.CurrentUser;
 import com.helpmycity.security.UserPrincipal;
@@ -16,14 +20,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Qualifier("userRepository")
     @Autowired
@@ -70,6 +82,36 @@ public class UserController {
         UserProfile userProfile = new UserProfile(user.getId(), user.getEmail(), user.getUsername(), user.getName(), user.getLastName(), null, user.getRoles());
 
         return userProfile;
+    }
+
+    @PostMapping("/update_user")
+    public Object setRole(@RequestParam UserProfile profile) {
+        if (userRepository.existsById(profile.getId())) {
+            User user = userRepository.findById(profile.getId()).orElseThrow(() -> new RuntimeException("User not found"));
+
+
+            Role userRole = roleRepository.findByRole(RoleName.getRole(profile.getRoles().iterator().next().getRole().name()))
+                    .orElseThrow(() -> new RuntimeException("User Role not set."));
+
+            user.setRoles(Collections.singleton(userRole));
+
+            User result = userRepository.save(user);
+
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentContextPath().path("/users/{email}")
+                    .buildAndExpand(result.getEmail()).toUri();
+
+            return ResponseEntity.created(location).body(new ApiResponse(true,
+                    "User update successfully"));
+
+        } else {
+
+            return new ResponseEntity(
+                    new ApiResponse(false, "internal error!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+
     }
 
     @GetMapping("/{username}/reclamations")
